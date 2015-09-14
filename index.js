754be0a2abd966583dadc753af60a2ae417c7e56
Max <me@maxlloyd.no>
2015-09-14 22:05:31 +0200

    fix

--- index.js
+++ index.js
@@ -6,55 +6,60 @@ var app = express();
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 var cheerio = require('cheerio');
-var subReddits = jsonfile.readFileSync('links.json').names;
+var subReddits = ['memes', 'wtf', 'images', 'gifs', 'pepe'];
+
+
 
 
 function scrapeSubReddit(name, filename) {
   request('https://www.reddit.com/r/' + name, function(error, response, body) {
     if (!error) {
 
-
       var $ = cheerio.load(body);
-      var links = Object.create(null);
-
-      console.log(colors.gray('Scraping subreddit with name: ' + colors.cyan(name)));
+      var links = [];
       var containsImgs = false;
       var index = 0;
+
+      console.log(colors.gray('Scraping subreddit with name: ' + colors.cyan(name)));
       $('.thing a').each(function(i, elem) {
         var href = elem.attribs.href;
         var isImgLink = /http:\/\/i\.imgur\.com\/(.)*/;
 
         if (isImgLink.test(href)) {
-
           mkdirp('scrapes/' + name + '/', function(err) {
             if (err) console.log("RIP");
           });
 
-          if(index > 0) links[index] = href;
-          jsonfile.writeFileSync(filename, links);
+          if(index % 2 === 0)
+            links.push(href);
+
           containsImgs = true;
           index++;
         }
       });
-      if(containsImgs) console.log(colors.gray('Subreddit with name ') + colors.magenta(name) + colors.gray(' contains images, saving to disk...'));
+      if (containsImgs) {
+        console.log(colors.gray('Subreddit with name ') + colors.magenta(name) + colors.gray(' contains images, saving to disk...'));
+        jsonfile.writeFileSync(filename, links);
+      } else {
+
+      }
     } else {
       console.log('Seems like something went wrong: ' + error + ', While trying to request subreddit with name: ' + name);
     }
   });
-
 }
+
 
 
 function scrape(subreddits) {
   var date = new Date();
   for (var i = 0; i < subreddits.length; i++) {
     (function(i) {
-      setTimeout(function() {
-        scrapeSubReddit(subreddits[i], 'scrapes/' + subreddits[i] + '/' + subreddits[i] + '-' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + '.json');
-      }, i * 300);
-    })(i);
+    setTimeout(function() {
+      scrapeSubReddit(subreddits[i], 'scrapes/' + subreddits[i] + '/' + subreddits[i] + '-' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + '.json');
+    }, i * 300);
+  })(i);
   }
-
 }
 
 
@@ -65,7 +70,6 @@ app.get('/api/:subreddit/:file', function(req, res) {
 });
 
 
-// Trying to think of ideas for this :/, feel free to make suggestions
 
 scrape(subReddits);
 
